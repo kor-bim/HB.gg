e8be0336478dde09faab0996652c38bc2938f91d
Yoon Han Bin <69673803+kor-bim@users.noreply.github.com>
2024-02-22 17:15:18 +0900

    Initial commit from Create Next App

--- src/app/_component/gnb-menu.tsx
+++ src/app/_component/gnb-menu.tsx
@@ -5,14 +5,17 @@ import { Button } from '@nextui-org/button'
 
 export const GnbMenu = () => {
   return (
-    <Navbar className="bg-transparent backdrop-filter-none" classNames={{ wrapper: 'w-full max-w-full' }}>
+    <Navbar className="fixed bg-transparent backdrop-filter-none" classNames={{ wrapper: 'w-full max-w-full' }}>
       <NavbarContent justify="start">
         <NavbarBrand className="flex gap-2">
           <Image src="/assets/logo-text.svg" alt="Logo" width={20} height={20} priority />
           <span>HB.GG</span>
         </NavbarBrand>
       </NavbarContent>
-      <NavbarContent className="flex px-8 h-12 rounded-full bg-content1 gap-12" justify="center">
+      <NavbarContent
+        className="absolute top-20 md:relative md:top-0 px-8 h-12 rounded-full bg-content1 gap-12"
+        justify="center"
+      >
         <NavbarItem>
           <Link color="foreground" href="#">
             홈
