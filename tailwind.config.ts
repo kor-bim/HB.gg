52f33c97784b953fe2a9af3b6ec40ae952695ac8
Yoon Han Bin <69673803+kor-bim@users.noreply.github.com>
2024-03-28 13:07:30 +0900

    💄 style: Tailwind CSS 설정 업데이트로 새로운 배경 이미지와 커서 추가
    'hero-pattern' 배경 이미지 설정을 유지하며, 'baron-pattern'이라는 새로운 배경 이미지 설정을 추가했습니다. 이 새로운 설정은 LOL 시즌 2024 키 비주얼을 사용하여 웹사이트의 시각적 매력을 더합니다.
    커스텀 커서 설정을 추가하여 사용자 인터페이스의 상호작용 부분에 새롭고 개성 있는 요소를 도입했습니다. 이는 사용자 경험을 향상시키는 데 기여합니다.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,12 @@ const config: Config = {
       },
       backgroundImage: {
         'hero-pattern':
-          'radial-gradient( 100% 150% at 50% -35%, rgba(14, 16, 21, 0) 0%, rgb(14, 16, 21, 0.75) 100% ), linear-gradient( 180deg, rgba(14, 16, 21, 0) -35%, rgba(14, 16, 21, 0.375) 100% ), linear-gradient( 180deg, rgba(14, 16, 21, 0.375) -35%, rgba(14, 16, 21, 0) 100% ), url(/assets/main_hero.png)'
+          'radial-gradient( 100% 150% at 50% -35%, rgba(14, 16, 21, 0) 0%, rgb(14, 16, 21, 0.75) 100% ), linear-gradient( 180deg, rgba(14, 16, 21, 0) -35%, rgba(14, 16, 21, 0.375) 100% ), linear-gradient( 180deg, rgba(14, 16, 21, 0.375) -35%, rgba(14, 16, 21, 0) 100% ), url(/assets/main_hero.png)',
+        'baron-pattern':
+          'radial-gradient( 100% 150% at 50% -35%, rgba(14, 16, 21, 0) 0%, rgb(14, 16, 21, 0.25) 100% ), linear-gradient( 180deg, rgba(14, 16, 21, 0) -35%, rgba(14, 16, 21, 0.175) 100% ), linear-gradient( 180deg, rgba(14, 16, 21, 0.175) -35%, rgba(14, 16, 21, 0) 100% ), url(/assets/lol-season2024keyvisual-riot.webp)'
+      },
+      cursor: {
+        pointer: 'url(/assets/cursor-pointer.png), pointer' // 'pointer'는 커스텀 클래스의 이름입니다
       }
     }
   },
